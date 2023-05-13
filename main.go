2c79467fe82eff67c58877b07df243e156e02d71
Siddharth Kundu <sk31395@gmail.com>
2023-05-13 17:44:04 +0530

    initial setup

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the env")
+	}
+
+	fmt.Println("PORT:", portString)
+}
